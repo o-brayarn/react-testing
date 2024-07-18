0f3717abceef14aa5d3c940369c7421bbba5a7db
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-18 22:31:59 +0300

    testing small functions with Jest

---
+++ src/Function.js
@@ -0,0 +1,5 @@
+const Function = (a) => {
+  return a * 2;
+};
+
+export default Function;
---
+++ src/Function.test.js
@@ -0,0 +1,4 @@
+import Function from "./Function";
+test("Multiplies by two", () => {
+  expect(Function(4)).toBe(8);
+});
